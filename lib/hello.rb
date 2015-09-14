7c8fe227a77f77829490829e0e654babfd8ac98e
danielstpaul <danielstpaul@hotmail.com>
2015-09-14 11:31:29 +0100

    Added shotgun gem and updated hello.rb to include picture

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div style='border: 3px dashed blue'>
+    <img src='https://www.petfinder.com/wp-content/uploads/2012/11/dog-how-to-select-your-new-best-friend-thinkstock99062463.jpg'>
+   <div>"
+end
+
+get '/secret' do
+  'This page is secret'
 end
