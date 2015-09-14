5e3e27b88a862cd213469478ef961ef1edd62948
danielstpaul <danielstpaul@hotmail.com>
2015-09-14 17:30:33 +0100

    Finished V1 up to final exercise

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -7,6 +7,7 @@ class BattleshipsWeb < Sinatra::Base
   end
 
   get '/new_game' do
+    @name = params[:name]
     erb :new_game
   end
 
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,6 +6,19 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'I enter my name' do
+    visit '/new_game'
+    fill_in 'name', :with => 'poohead'
+    click_button 'Submit'
+    expect(page).to have_content "Hello, poohead...welcome to your game of Battleships"
+  end
+
+  scenario 'I did not enter my name' do
+    visit '/new_game'
+    click_button 'Submit'
+    expect(page).to have_content "Please enter your name or you'll be called poohead"
+  end
 end
 # describe 'shooting at the board' do
 #   it 'results in :hit if there is a ship' do
