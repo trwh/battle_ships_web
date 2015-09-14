a0c3d79f992b0c13458fceb81551d5eca3bc3bef
danielstpaul <danielstpaul@hotmail.com>
2015-09-14 11:52:57 +0100

    Added erb file in views and updated hello.rb

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 
 get '/' do
-  "<div style='border: 3px dashed blue'>
-    <img src='https://www.petfinder.com/wp-content/uploads/2012/11/dog-how-to-select-your-new-best-friend-thinkstock99062463.jpg'>
-   <div>"
+  @name = %w(Dan).sample
+  erb :index #embedded Ruby
+end
+
+get '/hello' do
+  @visitor = params[:name]
+  erb :index
 end
 
 get '/secret' do
